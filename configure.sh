67b123c052f0ee2ce75297257617ffc5573711b6
Oleksii Zhurba <ozhurba@mirantis.com>
2018-11-12 12:24:40 -0600

    Pin tempest plugin versions
    Change-Id: I82d0d323da106b91c5120aadce89016f0656e732

--- configure.sh
+++ configure.sh
@@ -43,8 +43,8 @@ tempest_configuration () {
       export https_proxy=$PROXY
     fi
     rally verify create-verifier --name tempest_verifier_$sub_name --type tempest --source $TEMPEST_REPO --version $tempest_version
-    rally verify add-verifier-ext --source https://github.com/openstack/telemetry-tempest-plugin
-    rally verify add-verifier-ext --source https://github.com/openstack/heat-tempest-plugin
+    rally verify add-verifier-ext --version 7a4bff728fbd8629ec211669264ab645aa921e2b --source https://github.com/openstack/telemetry-tempest-plugin
+    rally verify add-verifier-ext --version 12b770e923060f5ef41358c37390a25be56634f0 --source https://github.com/openstack/heat-tempest-plugin
     pip install --force-reinstall python-cinderclient==3.2.0
     unset https_proxy
   fi
