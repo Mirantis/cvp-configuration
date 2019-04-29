8dcccda587ae421a0457d2dd399642a53c20f008
Oleksii Zhurba <ozhurba@mirantis.com>
2019-04-29 17:29:19 -0500

    [CVP] Align cvp-configuration 2018.4.0 branch with OS Pike release
    Change-Id: Id2cc15cd649c2d2474d6a71a9f00ae10eebe8171
    Related-PROD: PROD-29899

--- Dockerfile
+++ Dockerfile
@@ -4,7 +4,7 @@ SHELL ["/bin/bash", "-xec"]
 
 USER root
 
-RUN apt-get update; apt-get install -y inetutils-ping
+RUN apt-get update; apt-get install -y inetutils-ping curl wget
 
 WORKDIR /var/lib/
 
@@ -15,12 +15,14 @@ RUN git clone https://github.com/openstack/tempest && \
     popd;
 
 RUN git clone https://github.com/openstack/telemetry-tempest-plugin && \
-    pushd telemetry-tempest-plugin; pip install -r requirements.txt; \
+    pushd telemetry-tempest-plugin; git checkout 7a4bff728fbd8629ec211669264ab645aa921e2b; pip install -r requirements.txt; \
     popd;
 
 RUN git clone https://github.com/openstack/heat-tempest-plugin && \
-    pushd heat-tempest-plugin; git checkout 0.1.0; pip install -r requirements.txt; \
+    pushd heat-tempest-plugin; git checkout 12b770e923060f5ef41358c37390a25be56634f0; pip install -r requirements.txt; \
     popd;
+
+RUN pip install --force-reinstall python-cinderclient==3.2.0
 
 COPY rally/ /var/lib/cvp-configuration/rally
 COPY tempest/ /var/lib/cvp-configuration/tempest
--- configure.sh
+++ configure.sh
@@ -30,41 +30,38 @@ rally_configuration () {
   rally deployment config
 }
 
-additional_tempest_plugins (){
-if [ -n "${PROXY}" ]; then
-  export https_proxy=$PROXY
-fi
-#rally verify add-verifier-ext --source https://github.com/openstack/telemetry-tempest-plugin
-#rally verify add-verifier-ext --version 0.1.0 --source https://github.com/openstack/heat-tempest-plugin
-unset https_proxy
-}
-
 tempest_configuration () {
   sub_name=`date "+%H_%M_%S"`
-  # For 2018.4.0 we will use Tempest 15.0.0
-  tempest_version='15.0.0'
-  if [ -n "${OFFLINE}" ]; then
+  # default tempest version is now 17.2.0
+  tempest_version='17.2.0'
+  if [ "$PROXY" == "offline" ]; then
     rally verify create-verifier --name tempest_verifier_$sub_name --type tempest --source $TEMPEST_REPO --system-wide --version $tempest_version
-    cd /var/lib/
+    rally verify add-verifier-ext --source /var/lib/telemetry-tempest-plugin
+    rally verify add-verifier-ext --source /var/lib/heat-tempest-plugin
   else
     if [ -n "${PROXY}" ]; then
       export https_proxy=$PROXY
     fi
+    apt-get update; apt-get install -y iputils-ping curl wget
     rally verify create-verifier --name tempest_verifier_$sub_name --type tempest --source $TEMPEST_REPO --version $tempest_version
+    rally verify add-verifier-ext --version 7a4bff728fbd8629ec211669264ab645aa921e2b --source https://github.com/openstack/telemetry-tempest-plugin
+    rally verify add-verifier-ext --version 12b770e923060f5ef41358c37390a25be56634f0 --source https://github.com/openstack/heat-tempest-plugin
+    pip install --force-reinstall python-cinderclient==3.2.0
     unset https_proxy
   fi
   # supress tempest.conf display in console
   #rally verify configure-verifier --show
-  # Plugins will not be used for Tempest 15.0.0
-  #additional_tempest_plugins
-  pip install --force-reinstall python-cinderclient==3.2.0
 }
 
 quick_configuration () {
 current_path=$(pwd)
+# Remove this if you use local gerrit cvp-configuration repo
+if [ "$PROXY" == "offline" ]; then
+  current_path=/var/lib
+fi
 #image
 glance image-list | grep "\btestvm\b" 2>&1 >/dev/null || {
-    if [ -n "${PROXY}" ]; then
+    if [ -n "${PROXY}" ] && [ "$PROXY" -ne "offline" ]; then
       export http_proxy=$PROXY
     fi
     ls $current_path/cvp-configuration/cirros-0.3.4-x86_64-disk.img || wget http://download.cirros-cloud.net/0.3.4/cirros-0.3.4-x86_64-disk.img -O $current_path/cvp-configuration/cirros-0.3.4-x86_64-disk.img
@@ -86,8 +83,8 @@ if [ $shared_count -gt 1 ]; then
 fi
 if [ $shared_count -eq 0 ]; then
   echo "Let's create shared fixed net"
-  neutron net-create --shared tempest-net
-  neutron subnet-create --name tempest-subnet --gateway 192.168.0.1 --allocation-pool start=192.168.0.2,end=192.168.0.254 --ip-version 4 tempest-net 192.168.0.0/24
+  neutron net-create --shared fixed-net
+  neutron subnet-create --name fixed-subnet --gateway 192.168.0.1 --allocation-pool start=192.168.0.2,end=192.168.0.254 --ip-version 4 fixed-net 192.168.0.0/24
 fi
 FIXED_NET=$(neutron net-list -c name -c shared | grep True | awk '{print $2}' | tail -n 1)
 echo "Fixed net is: $FIXED_NET"
@@ -95,7 +92,9 @@ echo "Fixed net is: $FIXED_NET"
 sed -i 's/${IMAGE_REF2}/'$IMAGE_REF2'/g' $current_path/cvp-configuration/tempest/tempest_ext.conf
 sed -i 's/${FIXED_NET}/'$FIXED_NET'/g' $current_path/cvp-configuration/tempest/tempest_ext.conf
 sed -i 's/publicURL/'$TEMPEST_ENDPOINT_TYPE'/g' $current_path/cvp-configuration/tempest/tempest_ext.conf
-cat $current_path/cvp-configuration/tempest/tempest_ext.conf
+#supress tempest.conf display in console
+#cat $current_path/cvp-configuration/tempest/tempest_ext.conf
+cp $current_path/cvp-configuration/tempest/boot_config_none_env.yaml /home/rally/boot_config_none_env.yaml
 }
 
 if [ "$1" == "reconfigure" ]; then
@@ -111,7 +110,13 @@ rally_configuration
 if [ -n "${TEMPEST_REPO}" ]; then
     tempest_configuration
     quick_configuration
+    # If Opencontrail is deployed, use this command
+    #cat $current_path/cvp-configuration/tempest/skip-list-oc4.yaml >> $current_path/cvp-configuration/tempest/skip-list-pike.yaml
     rally verify configure-verifier --extend $current_path/cvp-configuration/tempest/tempest_ext.conf
+    rally verify configure-verifier --show
+    # If Barbican tempest plugin is installed, use this
+    # mkdir /etc/tempest
+    # rally verify configure-verifier --show | grep -v "rally.api" > /etc/tempest/tempest.conf
     # Add 2 additional tempest tests (live migration to all nodes + ssh to all nodes)
     # TBD
     #cat tempest/test_extension.py >> repo/tempest/scenario/test_server_multinode.py
