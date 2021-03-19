627a5ca56f961cd35a89ec7e69c3558ede6a51ab
Alex <osavatieiev@mirantis.com>
2021-03-19 09:37:59 -0500

    Updated K8s-related scripts to create Services pod in MOS child cluster
      Related-PROD: PROD-36128
    
    Change-Id: I9f6bd0c5096226fd9d7ba063ce881dee8655a704

---
+++ k8s/_init_pod.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+cd /artifacts
+# Installing prerequisites
+apt -y update
+apt -y install python3-pip vim git iperf3 mtr htop
+pip3 install rally-openstack python-neutronclient pyghmi
+
+# Prepare Rally
+rally db create
+
+# Create openstack env
+rally env create --from-sysenv --name openstack
+rally env check
+
+# Prepare rally for kubernetes
+bash res-files/k8s/gen_kubespec.sh ./mos-kubeconf.yaml
+git clone https://github.com/Mirantis/rally-plugins.git
+cd rally-plugins/
+pip3 install .
+rally plugin list | grep kubernetes
+
+# Configure kubernetes
+rally env create --name kubernetes --spec /artifacts/kubespec_generated.yaml
+rally env check
+cd /artifacts
