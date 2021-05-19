4a6e02d5aa7aa68deb88d438010730ae58e34e83
Ievgeniia Zadorozhna <izadorozhna@mirantis.com>
2021-05-19 17:57:16 +0300

    Added python-heatclient to be installed in ubuntu pod
    By default, the openstack client does not have the
    Heat extension, so cannot openrate the stacks.
    To fix this, need to install python-heatclient.
    
    Related-PROD: PROD-36128
    
    Change-Id: I5673694f8262881dd31840ac6c7bfcbd5bc84bdc

--- k8s/init-ubuntu.sh
+++ k8s/init-ubuntu.sh
@@ -3,7 +3,7 @@ cd /artifacts
 # Installing prerequisites
 apt -y update
 apt -y install python3-pip vim git iperf3 mtr htop iputils-ping traceroute tcpdump wget iproute2 curl
-pip3 install python-openstackclient python-neutronclient pyghmi
+pip3 install python-openstackclient python-neutronclient python-heatclient pyghmi
 
 mkdir /artifacts/cmp-check && cd /artifacts/cmp-check
 cp /artifacts/res-files/scripts/prepare.sh ./
