7b89e5b6623252db0d9848aa84901085cd79d64a
Ievgeniia Zadorozhna <izadorozhna@mirantis.com>
2021-06-10 17:32:07 +0300

    Added installation of octavia client in Ubuntu pod
    Let's add an octavia client installation to ubuntu pod,
    this will allow to operate "opentack loadbalancer" commands.
    
    Related-PROD: PROD-36128
    Change-Id: I420b38c41436f94e57a32469acf741d8294c39c2

--- k8s/init-ubuntu.sh
+++ k8s/init-ubuntu.sh
@@ -2,8 +2,10 @@
 cd /artifacts
 # Installing prerequisites
 apt -y update
+export TZ="America/Chicago"
+ln -snf /usr/share/zoneinfo/$TZ /etc/localtime && echo $TZ > /etc/timezone
 apt -y install python3-pip vim git iperf3 mtr htop iputils-ping traceroute tcpdump wget iproute2 curl
-pip3 install python-openstackclient python-neutronclient python-heatclient pyghmi
+pip3 install python-openstackclient python-neutronclient python-heatclient pyghmi python-octaviaclient
 
 mkdir /artifacts/cmp-check && cd /artifacts/cmp-check
 cp /artifacts/res-files/scripts/prepare.sh ./
