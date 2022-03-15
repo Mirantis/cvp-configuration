d08cbbd298d0a1d7b16834a0a79169b50583508e
Ievgeniia Zadorozhna <izadorozhna@mirantis.com>
2022-03-15 13:55:39 +0300

    Added cleaning of openstack services after Tempest run
    Added cleaning of servics at the cleanup script. The tempest
    services can appear after Tempest run (identity set).
    
    PROD: PROD-36829
    Change-Id: Ia35cd257ca8e8d9d4e37f383e74ce0e0871ac190

--- cleanup.sh
+++ cleanup.sh
@@ -191,6 +191,14 @@ function _clean_regions {
     _clean_and_flush
 }
 
+### Services
+function _clean_services {
+    services=( $(openstack service list -c Name -f value | grep ${mask}) )
+    echo "-> ${#services[@]} services containing '${mask}' found"
+    printf "%s\n" ${services[@]} | xargs -I{} echo service delete {} >>${cmds}
+    _clean_and_flush
+}
+
 ### Stacks
 function _clean_stacks {
     # By default openstack denies use of global_index for everyone.
@@ -242,6 +250,7 @@ _clean_sec_groups
 _clean_keypairs
 _clean_routers_and_networks
 _clean_regions
+_clean_services
 _clean_containers
 
 # project cleaning disabled by default
