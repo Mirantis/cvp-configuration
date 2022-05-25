1a21667696c46c43d5ea394f9d96b526a7ecf2a4
Alex <osavatieiev@mirantis.com>
2022-05-25 12:40:55 -0500

    Rally scenarios file placement updates
      Related-PROD: PROD-36856
    
    Change-Id: If121bfd18adb6b43d69f68bf2268a88e13db2c3d

--- k8s/rally-files/init-rally-kube.sh
+++ k8s/rally-files/init-rally-kube.sh
@@ -4,7 +4,7 @@ cd /artifacts
 # Configure kubernetes
 # Check and prepare kubespec file
 if [ ! -f /artifacts/kubespec_generated.yaml ]; then
-    sudo bash /opt/res-files/k8s/rally-files/gen_kubespec.sh /artifacts/mos-kubeconf.yaml
+    sudo bash /rally/rally-files/gen_kubespec.sh /artifacts/mos-kubeconfig.yaml
 fi
 # Create kubernetes env
 rally env create --name kubernetes --spec /artifacts/kubespec_generated.yaml
