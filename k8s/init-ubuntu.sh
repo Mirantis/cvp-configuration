101c5f6957fffaa2291e1a17b1c7c3f2c654c715
Ievgeniia Zadorozhna <izadorozhna@mirantis.com>
2022-07-26 17:12:53 +0300

    Added installation of 'iperf' package to the offline images and pods
    Added installation of 'iperf' package to the offline images due to
    the fix in mos-spt (https://gerrit.mcp.mirantis.com/c/mcp/mos-spt/+/138830):
    using iperf (v2) tool for the multiple threads test cases.
    
    Related-PROD: PROD-36943
    Change-Id: I157da5f37678b54cacdcdb38b3ea45aba347bd0a

--- k8s/init-ubuntu.sh
+++ k8s/init-ubuntu.sh
@@ -4,7 +4,7 @@ cd /artifacts
 apt -y update
 export TZ="America/Chicago"
 ln -snf /usr/share/zoneinfo/$TZ /etc/localtime && echo $TZ > /etc/timezone
-apt -y install python3-pip vim git iperf3 mtr htop iputils-ping traceroute tcpdump wget iproute2 curl
+apt -y install python3-pip vim git iperf3 iperf mtr htop iputils-ping traceroute tcpdump wget iproute2 curl
 pip3 install python-openstackclient python-neutronclient python-heatclient pyghmi python-octaviaclient
 
 mkdir /artifacts/cmp-check && cd /artifacts/cmp-check
