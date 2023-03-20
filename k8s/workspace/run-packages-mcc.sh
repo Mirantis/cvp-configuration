f0f745420efe7b37839504565bfefa92b5e43efc
Alex <osavatieiev@mirantis.com>
2023-03-20 16:58:19 -0500

    New versions of si-tests and mos-spt
     Related-PROD: PROD-36856
    
    Change-Id: I81656fea3bfe28678f309c27563b0f51726ecda1

--- k8s/workspace/run-packages-mcc.sh
+++ k8s/workspace/run-packages-mcc.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+cd /artifacts
+. env.sh
 . /opt/cfg-checker/.checkervenv/bin/activate
 mos-checker --ssh-direct --kube-config /artifacts/envs/mcc-kubeconfig.yaml --env-name $MY_CLIENTSHORTNAME-mcc --env-config /artifacts/envs/mcc-checker.env packages report --html $MY_CLIENTSHORTNAME-mcc-packages-01.html
 deactivate
--- k8s/workspace/run-packages-mos.sh
+++ k8s/workspace/run-packages-mos.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+cd /artifacts
+. env.sh
 . /opt/cfg-checker/.checkervenv/bin/activate
 mos-checker --ssh-direct --kube-config /artifacts/envs/mos-kubeconfig.yaml --env-name $MY_CLIENTSHORTNAME-mos --env-config /artifacts/envs/mos-checker.env packages report --html $MY_CLIENTSHORTNAME-mos-packages-01.html
 deactivate
